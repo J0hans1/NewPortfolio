57b2ca95e63c1c4a882ecf2686b475f94736d2a6
Markus A. R. Johansen <90006516+J0hans1@users.noreply.github.com>
2023-01-30 13:43:42 +0100

    added some scroll button style change, dosent work when

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -1,8 +1,23 @@
-import React, { useRef } from "react";
+import React, { useState, useEffect } from "react";
 import './css/navButton.css'
 
 const NavButton = (props) => {
-    const ref = useRef(null);
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        const pageElement = document.getElementById(props.pageId);
+        if (pageElement) {
+          setActive(window.pageYOffset >= pageElement.offsetTop);
+        }
+      };
+  
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, [props.pageId]);
+
 
     const handleClick = () => {
         const pageElement = document.getElementById(props.pageId);
@@ -15,9 +30,23 @@ const NavButton = (props) => {
       };
 
     return (
-        <div className="navlink-container" onClick={handleClick}>
-            <p>{props.text}</p>
-            <div></div>
+        <div 
+            className="navlink-container"
+            onClick={handleClick}
+        >
+            <p
+                style={{
+                    color: active ? "#e7e7e7" : "#e7e7e7bc"
+                    }}
+            >
+                {props.text}
+            </p>
+            <div
+                style={{
+                    width: active ? "100%" : "0%"
+                    }}
+            >
+            </div>
         </div>
     );
 }
